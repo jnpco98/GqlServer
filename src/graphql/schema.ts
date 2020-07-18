16ae258f0f3ea97b25493eb4ced4ae15550a53e7
ukiyo-roa <jnpco.98@gmail.com>
2020-07-19 01:44:52 +1000

    Forced create schema resolverGlob and authChecker params

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,7 +1,6 @@
 import { buildSchema, AuthChecker } from 'type-graphql';
 import { GraphQLSchema } from 'graphql';
 import { Request, Response } from 'express';
-import { isProduction } from '../utilities/environment';
 import path from 'path';
 
 let schema: GraphQLSchema;
@@ -11,13 +10,7 @@ export interface IContext {
   res: Response;
 }
 
-const DEFAULT_RESOLVER_GLOB = path.resolve(
-  __dirname,
-  '..',
-  `resolvers/**/!(*.test|*.spec).${isProduction() ? 'js' : 'ts'}`
-);
-
-export async function createSchema(resolverGlob: string = DEFAULT_RESOLVER_GLOB, authChecker?: AuthChecker<IContext>) {
+export async function createSchema(resolverGlob: string, authChecker: AuthChecker<IContext>) {
   if (!schema) {
     schema = await buildSchema({
       resolvers: [path.resolve(resolverGlob)],
